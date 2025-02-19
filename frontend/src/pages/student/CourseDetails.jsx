5cc9a770c251cc12916e20afab295fb954d4742c
Abhishek <abhishekmandavi8805@gmail.com>
2025-02-19 22:03:09 +0530

    commit

--- frontend/src/pages/student/CourseDetails.jsx
+++ frontend/src/pages/student/CourseDetails.jsx
@@ -14,7 +14,9 @@ const CourseDetails = () => {
 
   const [openSections , setOpenSections] = useState({})
 
-  const {allCourses, calculateRating, calculateChapterTime , calculateCourseDuration , calculateNoOfLectures} = useContext(AppContext)
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false)
+
+  const {allCourses, calculateRating, calculateChapterTime , calculateCourseDuration , calculateNoOfLectures , currency} = useContext(AppContext)
 
   const fetchCourseData = async () =>{
    const findCourse = allCourses.find(course => course._id === id)
@@ -108,8 +110,45 @@ const CourseDetails = () => {
           </div>
         </div>
 
-        <div>
-          <p>right</p>
+        {/*right side*/}
+        <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]'>
+          <img src={courseData.courseThumbnail} alt=''/>
+          <div className='p-5'>
+            <div className='flex items-center gap-2'>
+              <img className='w-3.5' src={assets.time_left_clock_icon} alt='time-left'/>
+              <p className='text-red-500'><span className='font-medium'>3 days</span> left at this price!</p>
+            </div>
+
+            <div className='flex gap-3 items-center pt-2'>
+              <p className='text-gray-800 md:text-4xl text-2xl font-semibold'>{currency}{(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2)}</p>
+              <p className='md:text-lg text-gray-500 line-through'>{currency}{courseData.coursePrice}</p>
+              <p className='md:text-lg text-gray-500'>{courseData.discount}% off</p>
+            </div>
+
+            <div className='flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500'>
+              <div className='flex items-center gap-1'>
+                <img  src={assets.star} alt='star icon'/>
+                <p>{calculateRating(courseData)}</p>
+              </div>
+
+
+              <div className='h-4 w-px bg-gray-500/40'></div>
+
+
+              <div className='flex items-center gap-1'>
+                <img  src={assets.time_clock_icon} alt='time icon'/>
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+
+              <div className='h-4 w-px bg-gray-500/40'></div>
+
+              <div className='flex items-center gap-1'>
+                <img  src={assets.lesson_icon} alt='time icon'/>
+                <p>{calculateNoOfLectures(courseData)} lessons</p>
+              </div>
+              
+            </div>
+          </div>
         </div>
       </div>
     </>
