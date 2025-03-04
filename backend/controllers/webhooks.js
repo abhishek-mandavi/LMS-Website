0878f12aa379cb9bb90e7946ff9834ec594ba414
Abhishek <abhishekmandavi8805@gmail.com>
2025-03-04 09:52:03 +0530

    commit

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -1,5 +1,8 @@
 import { Webhook } from "svix";
 import User from "../models/user.js";
+import Stripe from "stripe";
+import { Purchase } from "../models/Purchase.js";
+import Course from "../models/Course.js";
 
 export const clerkWebhooks = async (req,res) => {
   try {
@@ -48,5 +51,51 @@ export const clerkWebhooks = async (req,res) => {
     
   } catch (error) {
     res.json({success: false, message: error.message})
+  }
+}
+
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+export const stripeWebhooks = async(request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = Stripe.webhooks.constructEvent(request.body, process.env.STRIPE_WEBHOOK_SECRET);
+  } catch (error) {
+    response.status(400).send('Webhook Error: ${err.message}');
+  }
+
+  //Handle event
+  switch (event.type){
+    case 'payment_intent.succeeded':{
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id;
+
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId
+      })
+ 
+      const { purchaseId }  = session.data[0].metadata; 
+      
+      const purchaseData = await Purchase.findById(purchaseId)
+      const userData = await User.findById(purchaseData.userId)
+      const courseData = await Course.findById(purchaseData.courseId.toString())
+
+      courseData.enrolledStudents.push(userData)
+      await courseData.save()
+
+      userData.enrolledCourses.push()
+
+
+      break;
+    }
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      console.log('PaymentMethod was attached to a customer!');
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
   }
 }
